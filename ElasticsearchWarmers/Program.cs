89fb55005ade856bb248714617c908c65456e9d7
damienbod <damien_bod@hotmail.com>
2015-02-23 18:12:21 +0100

    init

---
+++ ElasticsearchWarmers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ElasticsearchWarmers.ElasticsearchProvider;
+
+namespace ElasticsearchWarmers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var searchProvider = new SearchProvider();
+
+			searchProvider.DeleteIndexIfItExists();
+
+			searchProvider.CreateIndexWithWarmerAndAddData();
+
+			foreach (var item in searchProvider.Search("mph"))
+			{
+				Console.WriteLine("Animal: {0}, Speed in mph: {1}", item.AnimalName, item.SpeedDoubleMph);
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
